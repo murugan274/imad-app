e6335e357f09e9021336bd2046523b035ab7399b
murugan274 <36512715+murugan274@users.noreply.github.com>
2018-02-17 18:44:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,12 +13,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one', function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
-app.get('/article-two', function (req,res){
-    res.send('article Two is requested and it will be served here');
-});
-app.get('/ui/madi.png', function (req, res) {
+get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
