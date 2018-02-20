da79530a483c51601fc97731123214ce03d6cd94
murugan274 <36512715+murugan274@users.noreply.github.com>
2018-02-20 22:39:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,30 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('counter');
+var counter =0;
+button.onclick = function () {
+    
+    //make are quest to the counter point
+    
+    //capture the response and store int in a variable
+    
+    //Render the variable in the correct span
+    counter = counter +1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+}
+//console.log('Loaded!');
 //change the text of main tesx of div
-var element = document.getElementById('main-text');
+//var element = document.getElementById('main-text');
 //element.innerHTML = 'New Value';
 
 // move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-  img.style.marginLeft = '100px';
-};
+//var img = document.getElementById('madi');
+//var marginLeft = 0;
+//function moveRight () {
+  //  marginLeft = marginLeft + 1;
+//    img.style.marginLeft = marginLeft + 'px';
+//}
+//img.onclick = function () {
+ //   var interval = setInterval(moveRight, 50);
+ // img.style.marginLeft = '100px';
+//};
