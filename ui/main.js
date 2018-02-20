5e26f1b13c57ef6251e920ca56ecb66e092f7b5d
murugan274 <36512715+murugan274@users.noreply.github.com>
2018-02-20 23:45:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,6 +22,24 @@ button.onclick = function () {
     request.open('GET', 'http://muruganitec.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+// make a rquest to the server and send the name
+//    request.open('GET', 'http://muruganitec.imad.hasura-app.io/counter', true);
+ //   request.send(null);
+//capture the list of name and render as a list
+var names = ['name1' , 'name2' , 'name3'];
+var list = '';
+for (var i=0; i,names.length; i++) {
+    list += '<li>' + names[i] + '</li>';
+}
+var ul = document.getElementById('namelist');
+ul.innerHTML= list;
+};
 //console.log('Loaded!');
 //change the text of main tesx of div
 //var element = document.getElementById('main-text');
