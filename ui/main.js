3724cb403aa8d4e8e7b5bb24745d815cc69cb407
murugan274 <36512715+murugan274@users.noreply.github.com>
2018-02-20 22:25:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 //change the text of main tesx of div
 var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
+//element.innerHTML = 'New Value';
 
 // move the image
 var img = document.getElementById('madi');
