9bc61d1da7d970cb9a2679fb669c9947b6f36635
murugan274 <36512715+murugan274@users.noreply.github.com>
2018-02-21 08:53:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,18 +28,35 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
+    //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store int in a variable
+    request.onreadystatechange = function () {
+    if (request.readyState === XMLHttpRequest.DONE) {
+        //take action
+        if (request.status === 200) {
+        var names = req.responseText;
+        names = JSON.parse(names);
+        var list = '';
+        for (var i=0; i < names.length; i++) {
+            list += '<li>' + names[i] + '</li>';
+        }
+        var ul = document.getElementById('namelist');
+        ul.innerHTML = list;        }
+    }
+    //not done yet
+};
+    //make the request
+    request.open('GET', 'http://muruganitec.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+};
 // make a rquest to the server and send the name
 //    request.open('GET', 'http://muruganitec.imad.hasura-app.io/counter', true);
  //   request.send(null);
 //capture the list of name and render as a list
-var names = ['name1' , 'name2' , 'name3'];
-var list = '';
-for (var i=0; i < names.length; i++) {
-    list += '<li>' + names[i] + '</li>';
-}
-var ul = document.getElementById('namelist');
-ul.innerHTML = list;
-};
+
+//};
 //console.log('Loaded!');
 //change the text of main tesx of div
 //var element = document.getElementById('main-text');
