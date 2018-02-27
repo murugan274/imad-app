02b1543b53197f1bf096bcb6db49e9d3961ec426
murugan274 <36512715+murugan274@users.noreply.github.com>
2018-02-27 23:37:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool =require('pg').pool;
+var config = {
+    user: 'murugan.itec',
+    database: 'murugan.itec',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -65,6 +73,20 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+var pool = new pool(config);
+app.get('/test-db', function (req, res) {
+    //make a select request
+    pool.query('SELECT * FROM testm', function(err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+    }        else {
+                res.send(JSON.stringify(result));
+            }
+       });
+    });
+    //return a response with a result
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
