acfb6764bc375f740cb60d90c512be04c3fbd43c
murugan274 <36512715+murugan274@users.noreply.github.com>
2018-03-02 21:34:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,10 +119,10 @@ app.get('/articles/:articleName', function (req, res){
             res.status(500).send(err.toString());
     }        else {
                 if (result.rows.length === 0){
-                    res.status(400).send('Article not found');
+                    res.status(404).send('Article not found');
                 } else
                 {
-                var articleData = result.rows(0);
+                var articleData = result.rows[0];
                 res.send(createTemplate(articleData));
                }
     }
