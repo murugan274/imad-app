a37ce346e45b8c6989670f0d62f936fd69a5315f
murugan274 <36512715+murugan274@users.noreply.github.com>
2018-03-02 21:43:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ var htmlTemplate =`<html>
         ${heading}
         </h1>
         <div>
-    ${date}
+    ${date.toDateString()}
         </div>        
         <div>
   ${content}             
